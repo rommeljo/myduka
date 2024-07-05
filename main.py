79e69be08b921e2dc5ec9726a32f9ce91cbef492
rommeljo <johnrommel031@gmail.com>
2024-07-05 13:17:01 +0300

    initial push

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from flask import Flask,render_template,request,url_for,redirect,flash
+from flask import Flask,render_template,request,url_for,redirect,flash,session
 from dbservice import get_data,profits_products,sales_perday,sales_today,topprofit_product,check_eamil_password
 from dbservice import insert_products,total_sales,profits_perday,topselling_product,insert_users
 from dbservice import insert_sales,sales_product,total_profits,profit_today,last_10,check_email
 from datetime import datetime
 # create flask instance
 app=Flask(__name__)
-app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+app.secret_key = 'NALI'
 # first route
 @app.route('/')
 def home():
@@ -13,14 +13,25 @@ def home():
 
 @app.route('/products',methods=['GET'])
 def products():
-    prods=get_data(" products")
+    if "email" in session:
+        prods=get_data(" products")
+    else:
+        flash('Please login to access this page','danger')
+        return redirect(url_for('login'))
+   
     return render_template('products.html',products=prods)
    
 
 @app.route('/sales')
 def sales():
-    prod=get_data("products")
-    sales=get_data("sales")
+    if "email" in session:
+
+        prod=get_data("products")
+        sales=get_data("sales")
+    else:
+        flash('Please login to access this page','danger')
+        return redirect(url_for('login'))
+    
     return render_template('sales.html',sales=sales,prodducts=prod)
     
     
@@ -28,39 +39,43 @@ def sales():
 
 @app.route('/dashboard')
 def dashboard():
-    salep=sales_product()
-    p_name=[]
-    p_sale=[]
-    for i in salep:
-        p_name.append(i[0])
-        p_sale.append(i[1])
+        if "email" in session:
+            salep=sales_product()
+            p_name=[]
+            p_sale=[]
+            for i in salep:
+                p_name.append(i[0])
+                p_sale.append(i[1])
 
-    product_name=[]
-    profits_prod=[]
-    prof=profits_products()
-    for i in prof:
-        product_name.append(i[0])
-        profits_prod.append(i[1])
-    prof1=[]
-    day1=[]
-    day_prof=profits_perday()
-    for i in day_prof:
-        prof1.append(str(i[0]))
-        day1.append(i[1])
-    sales1=[]
-    date2=[]
-    day_persales=sales_perday()
-    for i in day_persales:
-        sales1.append(str(i[0]))
-        date2.append(i[1])
-    top_pro=topselling_product()
-    tatal_s=total_sales()
-    total_p=total_profits()
-    today_sales=sales_today()
-    top_profit=topprofit_product()
-    today_profit=profit_today()
-    latest=last_10()
-    return render_template('dashboard.html',p_name=p_name,p_sale=p_sale,tatal_s=tatal_s,product_name=product_name,profits_prod=profits_prod,total_p=total_p,prof1=prof1,day1=day1,sales1=sales1,date2=date2,today_sales=today_sales,top_pro=top_pro,top_profit=top_profit,today_profit=today_profit,latest=latest)
+            product_name=[]
+            profits_prod=[]
+            prof=profits_products()
+            for i in prof:
+                product_name.append(i[0])
+                profits_prod.append(i[1])
+            prof1=[]
+            day1=[]
+            day_prof=profits_perday()
+            for i in day_prof:
+                prof1.append(str(i[0]))
+                day1.append(i[1])
+            sales1=[]
+            date2=[]
+            day_persales=sales_perday()
+            for i in day_persales:
+                sales1.append(str(i[0]))
+                date2.append(i[1])
+            top_pro=topselling_product()
+            tatal_s=total_sales()
+            total_p=total_profits()
+            today_sales=sales_today()
+            top_profit=topprofit_product()
+            today_profit=profit_today()
+            latest=last_10()
+        else:
+            flash('Please login to access this page','danger')
+            return redirect(url_for('login'))
+        return render_template('dashboard.html',p_name=p_name,p_sale=p_sale,tatal_s=tatal_s,product_name=product_name,profits_prod=profits_prod,total_p=total_p,prof1=prof1,day1=day1,sales1=sales1,date2=date2,today_sales=today_sales,top_pro=top_pro,top_profit=top_profit,today_profit=today_profit,latest=latest)
 
 
 
@@ -73,6 +88,7 @@ def add_product_route():
 
     product = (name, buyingprice, sellingprice, quantity)
     insert_products(product)
+    flash('Product added successfully','success')
     return redirect(url_for('products'))
 
 
@@ -84,6 +100,7 @@ def add_sales_route():
 
     sales=(productid,quantity,created_at)
     insert_sales(sales)
+    flash('Sales added successfully','success')
     return redirect(url_for('sales'))
 
 @app.route('/login',methods=["POST","GET"])
@@ -95,6 +112,7 @@ def login():
             if len(c_mail)==1:
                 cc=check_eamil_password(email,password)
                 if len(cc)==1:
+                 session['email']=email
                  flash("login successful",'success')
                  return redirect(url_for('dashboard'))
                 else:
@@ -122,6 +140,12 @@ def register():
            
         return render_template('register.html')
 
+
+
+@app.route('/logout')
+def logout():
+    session.pop('email',None)
+    return redirect(url_for('login'))
 
 # create 3 html files  and ensure 
 # all html files are bs enabled
